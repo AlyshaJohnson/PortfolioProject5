1db773c54980e7cae00155f15747215127071d70
AlyshaJohnson <alysha.johnson@live.co.uk>
2023-02-12 17:05:08 +0000

    downgrade django version

--- whirl/settings.py
+++ whirl/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 if os.path.exists('env.py'):
     import env
@@ -30,7 +31,6 @@ SECRET_KEY = 'django-insecure-i5vmk$7$b2r=!ocr@h@&pc(+(3^mtfy9f^%((l72r7u-ozu&r*
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -93,12 +93,18 @@ WSGI_APPLICATION = 'whirl.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DEV' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    }
+    print('done')
 
 # cloudinary storage
 
